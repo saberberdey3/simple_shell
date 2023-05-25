5b9113b71a6d1186eccac56579cd95783165b57f
Nissrinb <nberdey.123@gmail.com>
2023-05-24 17:22:25 -0700

    Fixes

--- my_non_interactiv.c
+++ my_non_interactiv.c
@@ -1,28 +1,24 @@
-#include "my_shell.h"
-
+#include "shell.h"
 /**
- * non_interactive_mode - the program in the non interactive mode.
- *
- * This function handles the non-interactive mode of the shell. It
- * takes a stream of input and executes the commands that are
- * found in the stream.
+ * my_non_interactive_mode - the program in the non interactive mode.
  */
 void my_non_interactive_mode(void)
 {
-char *line;
-char **cmds;
-int err_check = 0;
+	char *line;
+	char **cmds;
+	int err_check = 0;
+	int running = 1;
 
 	while (1)
 	{
-	line = read_stream();
-	cmds = tokeniz(line);
-	err_check = executcmd(cmds);
-	if (err_check > 0)
-	{
-	error(err_check, cmds, running);
+		line = read_stream();
+		cmds = tokeniz(line);
+		err_check = excutcmd(cmds);
+		if (err_check > 0)
+		{
+			error(err_check, cmds, running);
+		}
 	}
-}
-free(line);
-free(cmds);
+	free(line);
+	free(cmds);
 }
